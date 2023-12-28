157d22122b2d31c152d5bc88f5e1836925b0b671
Chris Selig <chris.selig@bidamia.ca>
2023-12-28 13:07:42 -0700

    downloaded player bios

--- 02_analysis/analysis.R
+++ 02_analysis/analysis.R
@@ -10,15 +10,16 @@ library(duckdb)
 # library(odbc)
 
 
-# Download data
+# Download data ----
 
-hof_list_raw <- rvest::read_html('https://en.wikipedia.org/wiki/List_of_members_of_the_Baseball_Hall_of_Fame') |> 
+#0.1 List of Hall of Famer Data ----
+raw <- rvest::read_html('https://en.wikipedia.org/wiki/List_of_members_of_the_Baseball_Hall_of_Fame') |> 
     rvest::html_nodes('table') |> 
     rvest::html_table() |>
     (\(.) .[[3]])() |> 
     janitor::clean_names()
 
-
+# 0.2 Baseball Computer Data ----
 # This duckdb database comes from: https://baseball.computer/
 # Create a database file on disk
 con <- dbConnect(duckdb(), dbdir = "retro_baseball_stats.db")
@@ -41,11 +42,29 @@ dbExecute(con, "USE main_models")
 # metrics_player_season_league_pitching
 # metrics_player_season_league_fielding
 
+# Park factors contains park factors calculated using a batter-pitcher-matched-pair methodology (and a more standard aggregate methodology as a fallback for years with insufficient data).
+# park_factors 
 
 # Let's find season-level statistics for all pitchers and put it in a DataFrame
 player_career_pitching_raw <- dbGetQuery(con, "SELECT * FROM metrics_player_career_pitching")
 player_career_offense_raw <- dbGetQuery(con, "SELECT * FROM metrics_player_career_offense")
 player_career_fielding_raw <- dbGetQuery(con, "SELECT * FROM metrics_player_career_fielding")
+
+# 0.3 Player Info and other Reference Data ----
+# https://github.com/chadwickbureau/retrosheet/tree/official
+urls <- list(
+    'https://github.com/chadwickbureau/retrosheet/blob/40d79e757aa348f6e11303e8bd1d2a966bfe877f/reference/biofile.csv', # player bio
+    'https://github.com/chadwickbureau/retrosheet/blob/40d79e757aa348f6e11303e8bd1d2a966bfe877f/reference/coaches.csv', # coaches
+    'https://github.com/chadwickbureau/retrosheet/blob/40d79e757aa348f6e11303e8bd1d2a966bfe877f/reference/relatives.csv', # relatives
+    'https://github.com/chadwickbureau/retrosheet/blob/40d79e757aa348f6e11303e8bd1d2a966bfe877f/reference/teams.csv' # teams names
+    )
+
+urls |>
+    purrr::map(
+        \(x)  download.file(x, destfile = file.path("00_data/", basename(x)))
+        )
+
+    
 
 # Get list of players from duckdb database
 
