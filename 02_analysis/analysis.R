2f31092846c02f6cc5eab8f211fa0918ee6a1a2a
Chris Selig <chris.selig@bidamia.ca>
2024-01-11 19:15:12 -0700

    added player reference data

--- 02_analysis/analysis.R
+++ 02_analysis/analysis.R
@@ -41,8 +41,36 @@ urls |>
         \(x)  download.file(x, destfile = file.path("00_data_raw/", basename(x)))
         )
 
-# 1.3 Baseball Reference Data ----
+# 1.3 Download Player Ids ----
+
+# Create function that doesn't error out if the file doesn't exist
+
+safety_function <- function(x) {
+    tryCatch(
+        download.file(x, destfile = file.path("00_data_raw/", basename(x))),
+        error = function(e) e
+    )
+}
+
+numbers <- c('0','1','2','3','4','5','6','7','8','9')
+
+# Download people files that have a number in name
+paste0('https://raw.githubusercontent.com/chadwickbureau/register/master/data/people-',numbers,'.csv') |>
+    purrr::map(
+        \(x)  safety_function(x)
+    )
+
+
+# Download files that have letters in the name
+paste0('https://raw.githubusercontent.com/chadwickbureau/register/master/data/people-',letters,'.csv') |>
+    purrr::map(
+        \(x)  safety_function(x)
+    )
+
+# 1.4 Baseball Reference Data ----
 # This is daily data so needs a pipeline that runs daily
+
+
 
 # 2.0 Read In Data ----
 
@@ -125,6 +153,8 @@ player_season_offense <- dbGetQuery(
     con, 
     "SELECT
         player_id,
+        season,
+        league
         plate_appearances,
         at_bats,
         hits,
@@ -204,6 +234,20 @@ coaches_raw <- reference_data_list[[2]]
 relatives_raw <- reference_data_list[[3]]
 teams_raw <- reference_data_list[[4]]
 
+# 2.4 Fangraph fWAR data ----
+fg_war_raw <- readr::read_csv("00_data_raw/fWAR_2023.csv")
+
+# 2.5 Chadwickbureau Player Reference id Data ----
+
+# read all files that have people- in name
+chadwickbureau_player_reference_raw <- list.files("00_data_raw/", pattern = "people-") |>
+    purrr::map(
+        \(x) readr::read_csv(file.path("00_data_raw/", x), col_types = readr::cols(.default = "c"))
+        ) |>
+    purrr::reduce(
+        dplyr::bind_rows
+        )
+
 # 3.0 Clean Data & Combine Data ----
 
 # 3.1 Clean Reference Data ----
@@ -217,15 +261,81 @@ relatives <- relatives_raw |>
 teams <- teams_raw |> 
     janitor::clean_names()
 
+# 3.2 Clean Fangraph fWAR Data ----
+fg_war <- fg_war_raw |> 
+    janitor::clean_names()
+    # 
+# fg_names <- fg_war |> 
+#     select(name)
+# 
+# player_bio_names <- player_bio |> 
+#     select(player_id, first, last, nickname) |> 
+#     mutate(name = paste(nickname, last, sep = " ")) #|> 
+#     # select(-first, -last)
+# 
+# matched_names <- fg_names |> 
+#     inner_join(player_bio_names, by = "name") |> 
+#     select(name, player_id)
+# 
+# unmatched_names <- fg_names |> 
+#     anti_join(player_bio_names, by = "name") |> 
+#     select(name) |>
+#     distinct(name)
+# 
+# fuzzy_matched_names <- unmatched_names |> 
+#     mutate(
+#         player_id = stringdist::amatch(name, player_bio_names$name, maxDist = 3),
+#         player_id = as.character(player_id)
+#     )
+# 
+# # Remove suffix to match
+# split_suffix_match_test <- fuzzy_matched_names |>
+#     filter(is.na(player_id)) |> 
+#     # remove rows with (Unknown) in name
+#     filter(!grepl("(Unknown)", name)) |> 
+#     # Create new column that finds Sr. or Jr. in name column. Remove from name column and add to "suffix" column
+#     mutate(
+#         suffix = stringr::str_extract(name, "(Sr.|Jr.)"),
+#         name = stringr::str_remove(name, "(Sr.|Jr.)")
+#     ) |> 
+#     mutate(
+#         player_id = stringdist::amatch(name, player_bio_names$name, maxDist = 3)    
+#     )
+#     
+# suffix_match <- split_suffix_match_test |> 
+#     filter(!is.na(player_id)) |> 
+#     select(name, suffix, player_id) |> 
+#     mutate(
+#         name = paste(name, suffix, sep = " "),
+#         player_id = as.character(player_id)
+#     )
+# 
+# unmatched_names <- split_suffix_match_test |> 
+#     filter(is.na(player_id))
+# 
+# 
+# 
+# # Combine all player_id tables together
+# player_id_matched_names <- matched_names |>
+#     bind_rows(fuzzy_matched_names) |>
+#     bind_rows(suffix_match) |> 
+#     select(-suffix) |> 
+#     distinct(name,player_id)
+    
+# Combine updates names with fg_war
+fg_war |>
+    left_join(player_id_matched_names, by = "name") |> View()
+    # inner_join(player_bio_names, by = "player_id") |> 
+    # select(name, player_id)
 
 # 9.0 Reference Data to parquet ----
-arrow::write_parquet(player_bio_tidy, "00_data_clean/player_bio.parquet")
+arrow::write_parquet(player_bio, "00_data_clean/player_bio.parquet")
 # arrow::write_parquet(coaches_raw, "00_data_clean/coaches.parquet")
-arrow::write_parquet(relatives_tidy, "00_data_clean/relatives.parquet")
-arrow::write_parquet(teams_tidy, "00_data_clean/teams.parquet")
+arrow::write_parquet(relatives, "00_data_clean/relatives.parquet")
+arrow::write_parquet(teams, "00_data_clean/teams.parquet")
 
 # 9.2 Career Level Stats to parquet ----
-arrow::write_parquet(player_career_offense_raw, "00_data_clean/player_career_offense.parquet")
+arrow::write_parquet(player_career_offense, "00_data_clean/player_career_offense.parquet")
 # arrow::write_parquet(player_career_fielding_raw, "00_data_clean/player_career_fielding.parquet")
 # arrow::write_parquet(player_career_pitching_raw, "00_data_clean/player_career_pitching.parquet")
 
@@ -253,9 +363,9 @@ motherduck_con <- duckdb::dbConnect(duckdb(),paste0('md:baseball?motherduck_toke
 
 
 # Write data to table
-dbWriteTable(motherduck_con, "relatives", relatives_tidy,overwrite = TRUE)
+dbWriteTable(motherduck_con, "relatives", relatives,overwrite = TRUE)
 dbWriteTable(motherduck_con, "player_bio", player_bio,overwrite = TRUE)
-dbWriteTable(motherduck_con, "teams", teams_tidy,overwrite = TRUE)
+dbWriteTable(motherduck_con, "teams", teams,overwrite = TRUE)
 dbWriteTable(motherduck_con, "player_career_offense", player_career_offense,overwrite = TRUE)
 dbWriteTable(motherduck_con, "player_season_offense", player_season_offense,overwrite = TRUE)
 
